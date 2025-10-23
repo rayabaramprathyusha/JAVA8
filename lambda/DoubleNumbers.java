4fb91c9f99222f1d4f9e4bc21b7c96dd7d75949b
rayabaramprathyusha <rayabaramprathyusha2810@gmail.com>
2025-10-23 13:22:16 -0500

    Create DoubleNumbers.java

---
+++ lambda/DoubleNumbers.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class DoubleNumbers {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        numbers.forEach(n -> System.out.println(n*2));
+    }
+}
